a15bab9a994eade7c29cad57bb3760788126e63e
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2022-08-10 11:12:38 +0530

    Time: 2 ms (38.12%), Space: 42.1 MB (93.93%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
@@ -14,20 +14,42 @@
  * }
  */
 class Solution {
-    public TreeNode sortedArrayToBST(int[] nums) {
-        return solve(nums,0,nums.length - 1);
+    class Node{
+        int low ,up;
+        TreeNode t;
+        Node(int l, int p, TreeNode node){
+            low = l;
+            up = p;
+            t = node;
+        }
     }
-    
-    private TreeNode solve(int[] nums,int left , int right){
-         if(left > right) 
-             return null;
-        if(left == right)
-            return new TreeNode(nums[left]);
-        int mid = (left + right)/2;
-        TreeNode root = new TreeNode(nums[mid]);
-        root.left = solve(nums,left,mid - 1);
-        root.right = solve(nums,mid+1, right);
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return null;
+        Stack<Node> stack = new Stack<Node>();
+          // initialize
+        TreeNode root = new TreeNode(nums[(nums.length-1)/2]);
+        Node rootNode = new Node(0,nums.length-1,root);
+        stack.push(rootNode);
+        //iteration
+        while(!stack.isEmpty()){
+            Node node = stack.pop();
+            int middle = (node.low+ node.up)/2; //cut jalf for[low,up]
+            //[low, middle - 1]
+            if(middle - 1 >= node.low){
+                TreeNode leftnode = new TreeNode(nums[(middle - 1 + node.low )/ 2]);
+                node.t.left = leftnode;
+                Node left = new Node(node.low, middle - 1, leftnode);
+                stack.push(left);
+            }
+            //[middle + 1, up]
+            if(middle + 1 <= node.up){
+                TreeNode rightnode = new TreeNode(nums[(middle + 1 + node.up) / 2]);
+                node.t.right = rightnode;
+                Node right = new Node(middle + 1, node.up, rightnode);
+                stack.push(right);
+            }
+        }
         return root;
-        
     }
 }
