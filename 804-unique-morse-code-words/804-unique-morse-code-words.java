40f364be08e85bee1d9395f0a209073ce720a50c
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2022-08-17 18:01:02 +0530

    Time: 4 ms (52.93%), Space: 42.4 MB (36.11%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.java
+++ 804-unique-morse-code-words/804-unique-morse-code-words.java
@@ -1,14 +1,14 @@
 class Solution {
+    private String transform(String str) {
+        String s[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."},res="";
+        for(int i=0;i<str.length();i++)
+            res+=s[(int)str.charAt(i)-'a'];
+        return res;
+    }
+    
     public int uniqueMorseRepresentations(String[] words) {
-        String[] tokens = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        Set<String> set = new HashSet<>();
-        for(String word: words){
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < word.length();i++){
-                sb.append(tokens[word.charAt(i)-'a']);
-            }
-            set.add(sb.toString());
-        }
+        HashSet<String> set = new HashSet<String>();
+        for(String w : words) set.add(transform(w));
         return set.size();
     }
 }
