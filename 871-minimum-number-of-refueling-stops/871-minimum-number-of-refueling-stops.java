375535498754ca9516564c33988a52092f35f8ba
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2022-08-20 15:57:38 +0530

    Time: 6 ms (52.57%), Space: 49.1 MB (40.62%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
@@ -1,16 +1,19 @@
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
-        int N = stations.length;
-        long[] dp = new long[N + 1];
-        dp[0] = startFuel;
-        for(int i  = 0; i < N; ++i)
-            for(int t = i; t >= 0; --t)
-                if(dp[t] >= stations[i][0])
-                    dp[t+1] = Math.max(dp[t + 1], dp[t] + (long) stations[i][1]);
+        int N = stations.length, ans = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
+        for(int i = 0; i <= N; i++){
+            int distance = i == N ? target : stations[i][0];
+            
+            while(startFuel < distance){
+                if(pq.size() == 0)
+                    return - 1;
+                startFuel += pq.poll();
+                ans++;
+            }
+            if(i < N) pq.add(stations[i][1]);
         
-        for(int i = 0; i <= N; ++i)
-            if(dp[i] >= target)
-                return i;
-        return -1;
+        }
+        return ans;
     }
 }
