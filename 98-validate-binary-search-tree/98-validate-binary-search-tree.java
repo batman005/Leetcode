b297ea05de939740e7c6db1013c7455221560dd7
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2022-08-30 22:20:50 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (73.39%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -17,17 +17,15 @@ import java.util.Optional;
 
 public class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Optional.empty(), Optional.empty());
+       return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
     
-    private boolean isValidBST(TreeNode node, Optional<Integer> min, Optional<Integer> max) {
-        if (node == null) {
-            return true;
-        }
-        if ((min.isPresent() && node.val <= min.get()) || (max.isPresent() && node.val >= max.get())) {
-            return false;
-        }
-        Optional val = Optional.of(node.val);
-        return isValidBST(node.left, min, val) && isValidBST(node.right, val, max);
+    boolean isValidBST(TreeNode root, long minValue, long maxValue){
+        if(root == null) return true;
+        
+        if(root.val <= minValue || root.val >= maxValue) return false;
+        
+        return isValidBST(root.left, minValue, root.val) &&
+            isValidBST(root.right, root.val,maxValue);
     }
 }
