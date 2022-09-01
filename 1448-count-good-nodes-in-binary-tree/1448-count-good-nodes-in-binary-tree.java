31611b118f9abf1d48bb16ed6d6f97f253fbaa53
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2022-09-01 17:52:49 +0530

    Time: 3 ms (76.91%), Space: 59.5 MB (64.37%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -14,20 +14,30 @@
  * }
  */
 class Solution {
-    public int goodNodes(TreeNode root) {
-       return dfs(root, root.val);
+ public int goodNodes (TreeNode root) {
+		// a call to our self-made method
+        return goodNodesUtil (root, root.val);        
     }
     
-    private int dfs(TreeNode root, int max){
-        if(root == null) return 0;
+    public int goodNodesUtil (TreeNode root, int maxPathValue) {
         
-        int cnt = 0;
-        
-        if(root.val >= max){
-            max = root.val;
-            cnt++;
+		// initialise the number of good nodes in the subtree with THIS node as root
+		// if THIS node's value is greater than maxPathValue, count THIS as a good node
+		int count = 0;
+        if (root.val >= maxPathValue) {
+            count++;
+        }
+		
+		// Before recursive calls to the sub-trees, save up by checking for NULL
+		// Instead of updating maxPathValue, you can simply pass the maximum value
+		// Notice the second argument passed, it is the new maximum
+        if (root.left != null) {
+            count = count + goodNodesUtil(root.left, Math.max(root.val, maxPathValue));
+        }
+        if (root.right != null) {
+            count = count + goodNodesUtil(root.right, Math.max(root.val, maxPathValue));
         }
         
-        return cnt += dfs(root.left, max) + dfs(root.right, max);
+        return count;
     }
 }
