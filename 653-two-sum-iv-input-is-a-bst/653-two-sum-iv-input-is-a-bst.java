c9a9e5dc24369d4e38209ecac028cc94b682b3c2
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2022-10-09 09:56:27 +0530

    Time: 6 ms (72.26%), Space: 51.7 MB (42.89%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -14,40 +14,21 @@
  * }
  */
 class Solution {
-public boolean findTarget(TreeNode root, int k) {
-	List<Integer> sortedList = new ArrayList<>();
-	// collect the nodes in sorted order
-	collectNodes(root, sortedList);
-	int left = 0, right = sortedList.size() - 1;
-	// now use two pointers to check if there are two numbers that sum to k
-	// if the sum is greater than k then we know we need to replace a number
-	// the bigger value of the two numbers we sorted with a small number 
-	// so we decrement the right pointer. Similarly, if the sum is less than k
-	// then we know that we need to replace the smaller value with a larger value
-	// so we increment the left pointer.
-	while (left < right) {
-		int sum = sortedList.get(left) + sortedList.get(right);
-		if (sum > k) {
-			right--;
-		} else if (sum < k) {
-			left++;
-		} else {
-			return true;
-		}
-	}
-	return false;
-}
-// traverse tree inorder... search left portion add current nodes
-// val to list then vist right. This will lead to collecting node values in sorted order because
-// the tree is a Binary Search Tree with the given properies that all the elements on the left
-// of the current node will be less and all the elements to the right will be greater.
-public void collectNodes(TreeNode root, List<Integer> sortedList) {
-	if (root == null) {
-		return;
-	}
-	collectNodes(root.left, sortedList);
-	sortedList.add(root.val);
-	collectNodes(root.right, sortedList);
-}
+    public boolean findTarget(TreeNode root, int k) {
+        List<Integer> nums = new ArrayList<>();
+        inorder(root, nums);
+        for(int i = 0,  j  = nums.size() - 1; i <j;){
+            if(nums.get(i) + nums.get(j) == k) return true;
+            if(nums.get(i) + nums.get(j)< k) i++;
+            else j--;
+        }
+        return false;
+    }
     
+    public void inorder(TreeNode root, List<Integer> nums){
+        if(root == null) return;
+        inorder(root.left, nums);
+        nums.add(root.val);
+        inorder(root.right, nums);
+    }
 }
