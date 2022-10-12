1deb9a6c921968bc50892fc619e7bc95fe68c58d
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2022-10-12 18:53:22 +0530

    Time: 30 ms (55.87%), Space: 44.6 MB (62.79%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,19 +1,31 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n = nums.length;
-        Set<List<Integer>> result = new HashSet();
-        Arrays.sort(nums);
-        for(int k = 0; k < n - 3; k++){
-            for(int i = k + 1; i < n - 2; i++){
-                int low = i + 1; int high = n - 1;
-                while(low < high){
-                    long sum = (long)(nums[k] + nums[i]) + (long)(nums[low] + nums[high]);
-                    if(sum == target) result.add(Arrays.asList(nums[k], nums[i], nums[low++], nums[high--]));
-                    else if (sum < target) low++;
-                    else high--;
-                }
+        ArrayList<List<Integer>> res=new ArrayList<>();
+        Arrays.sort(nums);   // sort the array
+        if(nums.length<4)return res;
+        for(int i=0;i<nums.length-3;i++){
+            if(i>0 && nums[i]==nums[i-1])continue;  // avoid duplicates
+            for(int j=i+1;j<nums.length-2;j++){
+                if(j>i+1 && nums[j]==nums[j-1])continue;  //avoid duplicates
+                int l=j+1;
+            int r=nums.length-1;
+            while(l<r){
+                long sum=(long)nums[i]+nums[j]+nums[l]+nums[r];
+                if(sum==target){
+                    res.add(Arrays.asList(nums[i],nums[j],nums[l],nums[r]));
+					
+                    while(l<r && nums[l] == nums[l+1])l++; // avoid duplicates
+                    while(l<r && nums[r]== nums[r-1])r--;  //avoid duplicates
+                    l++;
+                    r--;
+                }else if(sum<target){
+                    l++;
+                }else r--;
+              }
             }
+            
         }
-        return new ArrayList<>(result);
+        return res;
+    
     }
 }
