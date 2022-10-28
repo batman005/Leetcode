c9d23207058e20d31b00b36fb121cba5fa819469
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2022-10-28 15:49:20 +0530

    Time: 7 ms (96.04%), Space: 44.8 MB (99.50%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,14 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+	//Map to store all the strings
        Map<String,List<String>> map = new HashMap<>();
         for(String s: strs){
+		 //sorting the string
             char[] charArray = s.toCharArray();
             Arrays.sort(charArray);
             String sorted = new String(charArray);
             
+			//putting sorted char which are not present in the group
             if(!map.containsKey(sorted)){
                 map.put(sorted, new LinkedList<String>());
             }
+			//adding it to the bucket of string which are anagrams
             map.get(sorted).add(s);
         }
         return new LinkedList<>(map.values());
