74d8d792dce40490a9ccba60e4d1d61462900684
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2022-11-22 13:02:29 +0530

    Time: 45 ms (72.89%), Space: 43.2 MB (54.68%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -6,7 +6,7 @@ class Solution {
         dp[0] = 0; 
         dp[1] = 1;
         
-        for(int i = 1; i <= n; i++)
+        for(int i = 2; i <= n; i++)
             for(int j = 1; j * j <= i; ++j)
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
         
