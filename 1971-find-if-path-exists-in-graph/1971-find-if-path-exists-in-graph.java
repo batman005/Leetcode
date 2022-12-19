46549ad77bb4327e9860ae87d1fd82311160dea5
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2022-12-19 23:04:18 +0530

    Time: 222 ms (45.87%), Space: 237.4 MB (20.53%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,28 +1,30 @@
 class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Map<Integer,List<Integer>> graph = new HashMap<>();
-        boolean[] seen = new boolean[n];
-        
-        for(int[] edge: edges){
-            int a = edge[0], b = edge[1];
-            graph.computeIfAbsent(a,val -> new ArrayList<Integer>()).add(b);
-            graph.computeIfAbsent(b, val-> new ArrayList<Integer>()).add(a);
-        }
-        return dfs(graph,seen,source,destination);
-    }
-    
-    private boolean dfs(Map<Integer,List<Integer>> graph, boolean[] seen, int currNode, int destination){
-        if(currNode == destination){
-            return true;
-        }
-        if(!seen[currNode]){
-            seen[currNode] = true;
-            for(int nextNode : graph.get(currNode)){
-                if(dfs(graph,seen,nextNode, destination)){
-                    return true;
-                }
+
+    public boolean hasPath(ArrayList<ArrayList<Integer>> adj , int start , int end , boolean[] vis){
+
+        if(start==end)  return true;
+        vis[start] = true;
+
+        for(int i : adj.get(start)){
+            if(!vis[i]){
+                if(hasPath(adj , i, end, vis))  return true;
             }
         }
         return false;
+
+    }
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        boolean[] vis = new boolean[n];
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] arr : edges){
+            adj.get(arr[0]).add(arr[1]);
+            adj.get(arr[1]).add(arr[0]);
+        }
+        boolean ans = hasPath(adj , source , destination, vis);
+        return ans;
     }
 }
