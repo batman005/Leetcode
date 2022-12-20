2fc2978abf56a024d0186600b095d781b39a275a
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2022-12-20 12:37:49 +0530

    Time: 2 ms (70.49%), Space: 41.8 MB (93.08%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -2,24 +2,24 @@ class Solution {
 
     // TC : O(n2)
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        Set<Integer> visitedRoomIDs = new HashSet<>();
-        Queue<Integer> qu =new LinkedList<>();
-        qu.offer(0);
-        visitedRoomIDs.add(0);
-        while(!qu.isEmpty()){
-            int size = qu.size();
-            while(size-->0){
-                Integer head = qu.poll();
-                for(Integer nextRoomId: rooms.get(head)){
-                    if(!visitedRoomIDs.contains(nextRoomId)){
-                        qu.offer(nextRoomId);
-                        visitedRoomIDs.add(nextRoomId);
-                    }
+        boolean[] seen = new boolean[rooms.size()];
+        seen[0] = true;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        
+        
+        while(!stack.isEmpty()){
+            
+            int node = stack.pop();
+            for(int neigh: rooms.get(node))
+                if(!seen[neigh]){
+                    seen[neigh] = true;
+                    stack.push(neigh);
                 }
-            }
         }
-
-        return rooms.size() == visitedRoomIDs.size();
+        for(boolean v: seen)
+            if(!v) return false;
+        return true;
 
     }
 }
