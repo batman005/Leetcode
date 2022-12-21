28d8bd50e75630638fb490f29ed4326d2437e82c
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2022-12-21 10:50:06 +0530

    Time: 75 ms (32.72%), Space: 74.1 MB (36.49%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,42 +1,34 @@
 class Solution {
-    //bfs
-    private boolean bfs(int source, Map<Integer, List<Integer>> adj, int[]color){
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(source);
-        color[source] = 0;
-        
-        while(!q.isEmpty()){
-            int node = q.poll();
-            if(!adj.containsKey(node))
-                continue;
-            for(int neighbor: adj.get(node)){
-                if(color[neighbor] == color[node])
+    private boolean dfs(int node, int nodeColor, Map<Integer, List<Integer>> adj, int[] color){
+        color[node] = nodeColor;
+        if(!adj.containsKey(node))
+            return true;
+        for(int neighbor: adj.get(node)){
+            if(color[neighbor] == color[node])
+                return false;
+            if(color[neighbor] == -1){
+                if(!dfs(neighbor, 1 - nodeColor, adj, color))
                     return false;
-                if(color[neighbor] == -1){
-                    color[neighbor] = 1 - color[node];
-                    q.add(neighbor);
-                }
             }
+
         }
         return true;
     }
     public boolean possibleBipartition(int n, int[][] dislikes) {
-        Map<Integer,List<Integer>> adj = new HashMap<>();
+        Map<Integer, List<Integer>> adj = new HashMap<>();
         for(int[] edge: dislikes){
             int a = edge[0], b = edge[1];
             adj.computeIfAbsent(a, value -> new ArrayList<Integer>()).add(b);
             adj.computeIfAbsent(b, value -> new ArrayList<Integer>()).add(a);
         }
-        int[] color =  new int[n + 1];
-        Arrays.fill(color, -1);//0 stands red & 1 stands blue
-        
+        int[] color = new int[n + 1];
+        Arrays.fill(color, -1);
         for(int i = 1; i <= n; i++){
             if(color[i] == -1){
-                if(!bfs(i, adj, color))
+                if(!dfs(i, 0, adj, color))
                     return false;
             }
         }
-        
         return true;
     }
 }
