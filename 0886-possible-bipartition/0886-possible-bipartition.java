eb77c5a3a1430fcf86ee1d8628ae0f20dbdb65df
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2022-12-21 10:34:17 +0530

    Time: 28 ms (80.79%), Space: 50.3 MB (80.35%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -0,0 +1,41 @@
+class Solution {
+    private boolean bfs(int source, Map<Integer, List<Integer>> adj, int[]color){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(source);
+        color[source] = 0;
+        
+        while(!q.isEmpty()){
+            int node = q.poll();
+            if(!adj.containsKey(node))
+                continue;
+            for(int neighbor: adj.get(node)){
+                if(color[neighbor] == color[node])
+                    return false;
+                if(color[neighbor] == -1){
+                    color[neighbor] = 1 - color[node];
+                    q.add(neighbor);
+                }
+            }
+        }
+        return true;
+    }
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+        Map<Integer,List<Integer>> adj = new HashMap<>();
+        for(int[] edge: dislikes){
+            int a = edge[0], b = edge[1];
+            adj.computeIfAbsent(a, value -> new ArrayList<Integer>()).add(b);
+            adj.computeIfAbsent(b, value -> new ArrayList<Integer>()).add(a);
+        }
+        int[] color =  new int[n + 1];
+        Arrays.fill(color, -1);//0 stands red & 1 stands blue
+        
+        for(int i = 1; i <= n; i++){
+            if(color[i] == -1){
+                if(!bfs(i, adj, color))
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+}
