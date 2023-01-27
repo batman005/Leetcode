39f21cc1e16ad79e63995de8abf2a734eb576caa
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-01-27 13:08:08 +0530

    Time: 86 ms (45.23%), Space: 71.5 MB (30.54%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.java
+++ 472-concatenated-words/472-concatenated-words.java
@@ -1,20 +1,32 @@
 class Solution {
+    private boolean dfs(final String word, int length, final boolean[] visited, final Set<String> dictionary) {
+        if (length == word.length()) {
+            return true;
+        }
+        if (visited[length]) {
+            return false;
+        }
+        visited[length] = true;
+        for (int i = word.length() - (length == 0 ? 1 : 0); i > length; --i) {
+            if (dictionary.contains(word.substring(length, i)) 
+                && dfs(word, i, visited, dictionary)) {
+                return true;
+            }
+            
+        }
+        return false;
+    }
+    
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
         final Set<String> dictionary = new HashSet<>(Arrays.asList(words));
         final List<String> answer = new ArrayList<>();
-        for(final String word: words){
+        for (final String word : words) {
             final int length = word.length();
-            final boolean[] dp = new boolean[length + 1];
-            dp[0] = true;
-            for(int i = 0; i <= length; ++i){
-                for(int j = (i == length ? 1 : 0); !dp[i] && j < i; ++j){
-                    dp[i] = dp[j] && dictionary.contains(word.substring(j, i));
-                }
-            }
-            if(dp[length]){
+            final boolean[] visited = new boolean[length];
+            if (dfs(word, 0, visited, dictionary)) {
                 answer.add(word);
             }
         }
-        return answer;
+        return answer;   
     }
 }
