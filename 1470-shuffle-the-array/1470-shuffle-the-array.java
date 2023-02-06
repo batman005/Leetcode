971575faf5d39b6584fc59ef75d77a1aeaae7c39
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-02-06 16:07:46 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (80.82%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,11 +1,23 @@
 class Solution {
-    //normal array traversal
     public int[] shuffle(int[] nums, int n) {
-        int[] result = new int[2 * n];
-        for(int i = 0; i < n; ++i){
-            result[2 * i] = nums[i];
-            result[2 * i + 1] = nums[n + i];
+        // Store each y(i) with respective x(i).
+        for (int i = n; i < 2 * n; ++i) {
+            int secondNum = nums[i] << 10;
+            nums[i - n] |= secondNum;
         }
-        return result;
+
+        // '0000000000 1111111111' in decimal.
+        int allOnes = (int) Math.pow(2, 10) - 1;
+
+        // We will start putting all numbers from the end, 
+        // as they are empty places.
+        for (int i = n - 1; i >= 0; --i) {
+            // Fetch both the numbers from the current index.
+            int secondNum = nums[i] >> 10;
+            int firstNum = nums[i] & allOnes;
+            nums[2 * i + 1] = secondNum;
+            nums[2 * i] = firstNum;
+        }
+        return nums;
     }
 }
