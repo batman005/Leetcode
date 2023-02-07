18d0b2a7c78c396bcea17b4147442dd73c52ea9f
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-02-07 09:53:19 +0530

    Time: 10 ms (89.00%), Space: 51.4 MB (76.16%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.java
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -1,28 +1,29 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        // We use a hash map 'basket' to store the number of each type of fruit.
-        Map<Integer, Integer> basket = new HashMap<>();
-        int left = 0, maxPicked = 0;
-        
-        // Add fruit from the right index (right) of the window.
-        for (int right = 0; right < fruits.length; ++right) {
-            basket.put(fruits[right], basket.getOrDefault(fruits[right], 0) + 1);
-
-            // If the current window has more than 2 types of fruit,
-            // we remove fruit from the left index (left) of the window,
-            // until the window has only 2 types of fruit.
-            while (basket.size() > 2) {
-                basket.put(fruits[left], basket.get(fruits[left]) - 1);
-                if (basket.get(fruits[left]) == 0)
-                    basket.remove(fruits[left]);
-                left++;
+        int last_fruit = -1;
+        int second_last_fruit = -1;
+        int last_fruit_count = 0;
+        int current_max = 0;
+        int max = 0;
+        for(Integer fruit: fruits){
+            if(fruit == last_fruit || fruit == second_last_fruit){
+                current_max += 1;
+            } else {
+                current_max = last_fruit_count + 1;
             }
             
-            // Update maxPicked.
-            maxPicked = Math.max(maxPicked, right - left + 1);
+            if(fruit == last_fruit){
+                last_fruit_count += 1;
+            } else {
+                last_fruit_count = 1;
+            }
+            if(fruit != last_fruit){
+                second_last_fruit = last_fruit;
+                last_fruit = fruit;
+            }
+            
+            max = Math.max(current_max, max);
         }
-        
-        // Return maxPicked as the maximum number of fruits we can collect.
-        return maxPicked;
+        return max;
     }
 }
