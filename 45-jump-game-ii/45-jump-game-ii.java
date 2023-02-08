0a336b6ebdf7c68f8bddd5ea487f6f16a7943af0
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-02-08 16:43:35 +0530

    Time: 1 ms (99.45%), Space: 42.6 MB (83.82%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -3,8 +3,7 @@ class Solution {
     // TC : O(n)
     // SC : O(1)
     public int jump(int[] nums) {
-        int level =0;
-        int divider = 0;
+        int level = 0,divider  = 0;
         int maxReachableIndex = 0;
         for(int i=0;i<nums.length;i++){
             if(i> divider){
