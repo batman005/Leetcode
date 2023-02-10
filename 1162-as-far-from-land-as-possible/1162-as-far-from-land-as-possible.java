80ad453bfc140905420fee615c7151eab9b561df
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-02-11 00:13:46 +0530

    Time: 15 ms (72.46%), Space: 43.6 MB (45.20%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -1,52 +1,31 @@
 class Solution {
-    // Four directions: Up, Down, Left and Right.
-    int[][] direction = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    public int maxDistance(int[][] matrix) {
+      Queue<int[]> queue = new LinkedList<>();
+      int[][] directions = new int[][] {{1,0}, {-1,0}, {0,1}, {0,-1}};
+      int m = matrix.length;
+      int n = matrix[0].length;
+      int max = Integer.MIN_VALUE; //if it is not possible return -1..
+      for(int i = 0; i < m; i++)
+          for(int j = 0; j < n; j++)
+              if(matrix[i][j] == 1){ //land
+                  matrix[i][j] = 0; //make the land 0 because we will sum it with child!
+                  queue.add(new int[]{i,j});
+              }
+              else
+                  matrix[i][j] = Integer.MIN_VALUE; //waters
 
-    public int maxDistance(int[][] grid) {
-        // A copy matrix of the grid to mark water cells as land once visited.
-        int[][] visited = new int[grid.length][grid[0].length];
-
-        // Insert all the land cells in the queue.
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                // Copy grid to the visited matrix.
-                visited[i][j] = grid[i][j];
-                if (grid[i][j] == 1) {
-                    q.offer(new Pair(i, j));
-                }
-            }
-        }
-
-        int distance = -1;
-        while (!q.isEmpty()) {
-            int qSize = q.size();
-
-            // Iterate over all the current cells in the queue.
-            while (qSize-- > 0) {
-                Pair<Integer, Integer> landCell = q.poll();
-
-                // From the current land cell, traverse to all the four directions
-                // and check if it is a water cell. If yes, convert it to land
-                // and add it to the queue.
-                for (int[] dir : direction) {
-                    int x = landCell.getKey() + dir[0];
-                    int y = landCell.getValue() + dir[1];
-
-                    if (x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && visited[x][y] == 0) {
-                        // Marking as 1 to avoid re-iterating it.
-                        visited[x][y] = 1;
-                        q.offer(new Pair(x, y));
-                    }
-                }
-            }
-
-            // After each iteration of queue elements, increment distance 
-            // as we covered 1 unit distance from all cells in every direction.
-            distance++;
-        }
-
-        // If the distance is 0, there is no water cell.
-        return distance == 0 ? -1 : distance;
+      while(!queue.isEmpty()){
+          int[] pos = queue.poll();
+          for(int[] dir: directions){
+              int x = pos[0] + dir[0];
+              int y = pos[1] + dir[1];
+              if( x >= 0 && y >= 0 && x < m && y < n && matrix[x][y] < 0){ //point must be valid and water (Integer.MIN_VALUE)
+                  queue.add(new int[]{x,y});
+                  matrix[x][y] = matrix[pos[0]][pos[1]] + 1;
+                  max = Math.max(max , matrix[x][y]);
+              }
+          }
+      }
+      return max == Integer.MIN_VALUE ? -1 : max;
     }
 }
