2a6edb18e1a556f3ab5213dc3ff502bc770900eb
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-02-12 17:53:19 +0530

    Time: 107 ms (41.35%), Space: 106.1 MB (48.72%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.java
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.java
@@ -1,30 +1,44 @@
 class Solution {
-    long fuel;
     
-    public long dfs(int node, int parent, Map<Integer, List<Integer>> adj, int seats){
-        int representatives = 1;
-        if (!adj.containsKey(node)){
-            return representatives;
-        }
-        for(int child: adj.get(node)){
-            if(child != parent){
-                representatives += dfs(child, node, adj, seats);
+    public long bfs(int n, Map<Integer, List<Integer>> adj,int[] degree, int seats){
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 1; i < n; i++){
+            if(degree[i] == 1){
+                q.offer(i);
             }
         }
-        if(node != 0){
-            fuel += Math.ceil((double) representatives / seats);
+        
+        int[] representatives = new int[n];
+        Arrays.fill(representatives,1);
+        long fuel = 0;
+        
+        while(!q.isEmpty()){
+            int node =  q.poll();
+            fuel += Math.ceil((double) representatives[node]/seats);
+            
+            for(int neighbor: adj.get(node)){
+                degree[neighbor]--;
+                representatives[neighbor] += representatives[node];
+                if(degree[neighbor] == 1 && neighbor != 0){
+                    q.offer(neighbor);
+                }
+            }
+            }
+        return fuel;
         }
-        return representatives;
-    }
+
     
     
     public long minimumFuelCost(int[][] roads, int seats) {
+        int n = roads.length + 1;
         Map<Integer, List<Integer>> adj = new HashMap<>();
+        int[] degree = new int[n];
         for(int[] road: roads){
             adj.computeIfAbsent(road[0], k-> new ArrayList<Integer>()).add(road[1]);
             adj.computeIfAbsent(road[1], k-> new ArrayList<Integer>()).add(road[0]);
+            degree[road[0]]++;
+            degree[road[1]]++;
         }
-        dfs(0, -1, adj, seats);
-        return fuel;
+        return bfs(n, adj,degree, seats);
     }
 }
