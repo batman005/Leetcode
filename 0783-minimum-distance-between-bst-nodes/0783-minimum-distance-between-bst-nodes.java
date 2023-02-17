57c9e05fa6fc71e93bf91eb93ea3451ddace14cd
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-02-17 11:34:40 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (83.93%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.java
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.java
@@ -14,26 +14,25 @@
  * }
  */
 class Solution {
-    List<Integer> inorderNodes = new ArrayList<>();
+    int minDistance = Integer.MAX_VALUE;
+    TreeNode prevValue;
     
     void inorderTraversal(TreeNode root){
         if(root == null){
             return;
         }
+        
         inorderTraversal(root.left);
-        inorderNodes.add(root.val);
-        inorderTraversal(root.right);
-    }
-    
-    public int minDiffInBST(TreeNode root) {
-        inorderTraversal(root);
         
-        int minDistance = Integer.MAX_VALUE;
-        
-        for(int i = 1; i  < inorderNodes.size(); i++){
-            minDistance = Math.min(minDistance, inorderNodes.get(i) - inorderNodes.get(i - 1));
+        if(prevValue != null){
+            minDistance = Math.min(minDistance, root.val - prevValue.val);
         }
         
+        prevValue = root;
+        inorderTraversal(root.right);
+    }
+    public int minDiffInBST(TreeNode root) {
+        inorderTraversal(root);
         return minDistance;
     }
-}
+};
