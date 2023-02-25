c48ef72d8ec866828d56b0971aa6b7488a134257
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-02-25 14:02:40 +0530

    Time: 1 ms (85.57%), Space: 42.3 MB (50.06%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,5 +1,13 @@
 class Solution {
+      //Sliding window
+        // Take a window of size 2 and compare the values.
+       //If the later value is greater that means a profit. 
+      // Store the profit in your result (accumulate the result).
+// If the later value is smaller that means a loss. So, we won't do that trade. Just move 1 index ahead.
+//	6. Repeat step 3 till the end of the array/list.
+//	7. Return the result.
     public int maxProfit(int[] prices) {
+      
           int left = 0;
           int right = 1;
           int maxProfit = 0;
@@ -9,7 +17,7 @@ class Solution {
                 if(profit > 0){
                     maxProfit += profit;
                 }
-                left++;;
+                left++;
             }else{
                 left  = right;
             }
