3e7e12229bc6604c5c0f1d64b6856ba9cee42a5e
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-02-27 13:27:31 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (38.55%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,28 +1,21 @@
 class Solution {
     public int search(int[] nums, int target) {
         // Set the left and right boundaries
-        int left = 0, right = nums.length - 1;
+        int left = 0, right = nums.length;
         
-        // Under this condition
-        while (left <= right) {
-            // Get the middle index and the middle value.
-            int mid = left + right  >> 1 ;
-            
-            // Case 1, return the middle index.
-            if (nums[mid] == target) {
-                return mid;
-            } 
-            // Case 2, discard the smaller half.
-            else if (nums[mid] < target) {
-                left = mid + 1;   
-            } 
-            // Case 3, discard the larger half.
-            else {
-                right = mid - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
         
-        // If we finish the search without finding target, return -1.
-        return -1;
+        if (left > 0 && nums[left - 1] == target) {
+            return left - 1;
+        } else {
+            return -1;
+        } 
     }
 }
