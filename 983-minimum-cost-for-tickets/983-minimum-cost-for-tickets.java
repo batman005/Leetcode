c089c9772a4695e58f9d6d3e1fae1b27c1337f71
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-03-28 12:38:33 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (58.39%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
@@ -1,32 +1,30 @@
 class Solution {
-    int[] costs;
+    int[] days, costs;
     Integer[] memo;
-    Set<Integer> dayset;
-
+    int[] durations = new int[]{1, 7, 30};
+    
     public int mincostTickets(int[] days, int[] costs) {
-        this.costs = costs;
-        memo = new Integer[366];
-        dayset = new HashSet();
-        for (int d: days) dayset.add(d);
-
-        return dp(1);
+       this.days = days;
+       this.costs = costs;
+       memo = new Integer[days.length];
+        
+       return dp(0);
     }
-
-    public int dp(int i) {
-        if (i > 365)
+    
+    public int dp(int i){
+        if(i >= days.length)
             return 0;
-        if (memo[i] != null)
+        if(memo[i] != null)
             return memo[i];
-
-        int ans;
-        if (dayset.contains(i)) {
-            ans = Math.min(dp(i+1) + costs[0],
-                               dp(i+7) + costs[1]);
-            ans = Math.min(ans, dp(i+30) + costs[2]);
-        } else {
-            ans = dp(i+1);
-        }
-
+        
+        int ans = Integer.MAX_VALUE;
+        int j = i;
+        for(int k = 0; k < 3; ++k){
+            while(j < days.length && days[j] < days[i] + durations[k])
+                      j++;
+            ans = Math.min(ans, dp(j) + costs[k]);
+            }
+        
         memo[i] = ans;
         return ans;
     }
