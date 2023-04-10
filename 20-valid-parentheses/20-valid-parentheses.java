78b6c7910e96734be56426f122514b767ef402e5
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-04-10 09:34:48 +0530

    Time: 2 ms (79.07%), Space: 40.6 MB (42.60%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,30 +1,31 @@
 class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(char c: s.toCharArray()){
-            switch(c){
-                case ')':
-                    if(!stack.isEmpty() && stack.peek().compareTo('(') == 0)
-                        stack.pop();
-                    else 
-                        stack.push(c);
-                    break;
-                case ']':
-                    if(!stack.isEmpty() && stack.peek().compareTo('[') == 0)
-                        stack.pop();
-                    else 
-                        stack.push(c);
-                    break;
-                case '}':
-                        if (!stack.empty() && stack.peek().compareTo('{') == 0)
-                            stack.pop();
-                        else
-                            stack.push(c);
-                        break;
-                default:
-                    stack.push(c);
+         public static boolean isValid(String s) {
+    // Inilization of Stack
+        Stack<Character> st = new Stack<>();
+    // Iterating through the given String
+        for (int i = 0; i < s.length(); i++) {
+    // Push Open Brackets in the Stack
+            if (s.charAt(i) == '{' || s.charAt(i) == '(' || s.charAt(i) == '[') {
+                st.push(s.charAt(i));
             }
+    // checking for the empty stack because if empty this means there is no open bracket at the start which is an invaild string.
+            else if(st.empty()){
+                return false;
+            }
+    // Check for the close brackets pair on the top and pop them
+            else if (st.peek() == '{' && s.charAt(i) == '}' ) {
+                st.pop();
+            } else if (st.peek() == '(' && s.charAt(i) == ')' ) {
+                st.pop();
+            } else if (st.peek() == '[' && s.charAt(i) == ']' ) {
+                st.pop();
+            }else{
+                return false;
+            }
+
         }
-        return stack.isEmpty();
+        return st.empty();
+
+
     }
 }
