9114d627da516fe5fd67cd74d490c00d9a16752a
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-05-02 13:03:32 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (19.32%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -1,14 +1,14 @@
 class Solution {
     public int arraySign(int[] nums) {
-        int countNegativeNumbers = 0;
+        int sign = 1;
         for(int num : nums){
             if(num == 0){
                 return 0;
             }
             if(num < 0){
-                countNegativeNumbers++;
+                sign = -1 * sign;
             }
         }
-        return countNegativeNumbers % 2 == 0 ? 1 : -1;
+        return sign;
     }
 }
