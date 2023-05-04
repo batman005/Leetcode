8b67acfc09c665e90279946c43fd8def43203cc1
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-05-04 13:10:14 +0530

    Time: 8 ms (84.09%), Space: 43.6 MB (6.82%) - LeetHub

--- 649-dota2-senate/649-dota2-senate.java
+++ 649-dota2-senate/649-dota2-senate.java
@@ -1,31 +1,37 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-           int n = senate.length();
+        int rCount = 0, dCount = 0;
         
-           Queue<Integer> rQueue = new LinkedList<>();
-           Queue<Integer> dQueue = new LinkedList<>();
+        int dFloatingBan  = 0, rFloatingBan = 0;
         
+        Queue<Character> q = new LinkedList<>();
+        for(char c : senate.toCharArray()){
+            q.add(c);
+            if(c == 'R') rCount++;
+            else dCount++;
+        }
         
-        for(int i = 0; i < n; i++){
-            if(senate.charAt(i) == 'R'){
-                rQueue.add(i);
+        while(rCount > 0 && dCount > 0){
+            char curr = q.poll();
+            
+            if(curr == 'D'){
+                if(dFloatingBan > 0){
+                    dFloatingBan--;
+                    dCount--;
+                } else {
+                    rFloatingBan++;
+                    q.add('D');
+                }
             } else {
-                dQueue.add(i);
+                if(rFloatingBan > 0){
+                    rFloatingBan--;
+                    rCount--;
+                } else{
+                    dFloatingBan++;
+                    q.add('R');
+                }
             }
         }
-        
-        while(!rQueue.isEmpty() && !dQueue.isEmpty()){
-            int rTurn = rQueue.poll();
-            int dTurn = dQueue.poll();
-            
-            
-            if(dTurn < rTurn){
-                dQueue.add(dTurn + n);
-            } else{
-                rQueue.add(rTurn + n);
-            }
-        }
-        
-        return rQueue.isEmpty() ? "Dire": "Radiant";
+             return rCount > 0 ? "Radiant" : "Dire";
     }
 }
