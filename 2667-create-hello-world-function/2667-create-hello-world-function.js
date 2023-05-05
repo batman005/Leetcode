d40d57d35f9cc7d58e2c4f2b96b938cc749a9483
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-05-05 13:48:06 +0530

    Time: 64 ms (100.00%), Space: 41.4 MB (100.00%) - LeetHub

--- 2667-create-hello-world-function/2667-create-hello-world-function.js
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -5,6 +5,8 @@ function createHelloWorld(...args){
     return () => "Hello World";
 }
 
+const hello = createHelloWorld();
+console.log(hello());
 /**
  * const f = createHelloWorld();
  * f(); // "Hello World"
