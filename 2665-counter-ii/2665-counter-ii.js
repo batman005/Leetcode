14c840bda3740f4292a97fc48e6a7954d7ec03e6
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-05-07 10:50:30 +0530

    Time: 64 ms (70.46%), Space: 44.9 MB (40.38%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -4,11 +4,17 @@
  */
 var createCounter = function(init) {
     let currentCount = init;
-    return {
-        increment: () => ++currentCount,
-        decrement: () => --currentCount,
-        reset: () => (currentCount = init),
+    function increment(){
+        return ++currentCount;
     }
+    function decrement(){
+        return --currentCount;
+    }
+    function  reset (){
+        return currentCount = init;
+    }
+    
+    return {increment, decrement, reset};
 };
 
 /**
