78f1ff7765d6d8706d4066fc6c39db5bac4a9197
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-05-09 13:22:48 +0530

    Time: 46 ms (97.92%), Space: 41.9 MB (60.17%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -4,11 +4,5 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    const newArr = [];
-    for(let i = 0; i < arr.length; ++i){
-        if(fn(arr[i], i)){
-            newArr.push(arr[i]);
-        }
-    }
-    return newArr;
+    return arr.filter(fn);
 };
