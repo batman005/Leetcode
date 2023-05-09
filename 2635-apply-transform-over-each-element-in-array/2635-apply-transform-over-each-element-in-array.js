43d2b03f096d0b9567b4c491a2ab346c84b11053
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-05-09 12:54:29 +0530

    Time: 49 ms (93.55%), Space: 42.2 MB (37.70%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,9 +4,9 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    const newArr = [];
-    for (let i = 0; i < arr.length; ++i) {
-        newArr[i] = fn(arr[i], i);
+    const newArr = new Array(arr.length);
+    for(const i in arr){
+        newArr[i] = fn(arr[i], Number(i));
     }
     return newArr;
 };
