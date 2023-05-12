2ed6dce4c4195b3f705572d6d07db6d27b6af751
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-05-12 12:49:38 +0530

    Time: 20 ms (37.04%), Space: 114.7 MB (27.51%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,18 +1,23 @@
 class Solution {
-    public long mostPoints(int[][] questions) {
-         int n = questions.length;
-         long[] dp = new long[n];
-         dp[n - 1] = questions[n - 1][0];
-        
-        for(int i = n - 2; i >= 0; --i){
-            dp[i] = questions[i][0];
-            int skip = questions[i][1];
-            if(i + skip + 1 < n){
-                dp[i] += dp[i + skip + 1];
-            }
-            
-            dp[i] = Math.max(dp[i], dp[i + 1]);
+    long dp[];
+    protected long dfs(int[][] questions, int i) {
+         if (i >= questions.length){
+             return 0;
+         }
+        if(dp[i] != 0){
+            return dp[i];
         }
-        return dp[0];
+        long points = questions[i][0];
+        int skip = questions[i][1];
+        
+        dp[i] = Math.max(points + dfs(questions, i + skip + 1), dfs(questions, i+ 1));
+        return dp[i];
     }
+    
+    public long mostPoints(int[][] questions){
+        int n= questions.length;
+        dp = new long[n];
+        return dfs(questions, 0);
+    }
+    
 }
