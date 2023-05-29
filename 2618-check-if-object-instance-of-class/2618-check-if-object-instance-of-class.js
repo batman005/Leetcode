36b7034694e5171104876ccbd90dcac91f9cbbe2
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-05-29 11:53:36 +0530

    Time: 110 ms (48.06%), Space: 51.4 MB (91.78%) - LeetHub

---
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Object} object
+ * @param {Function} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function (obj, classFunction) {
+  if (obj === null || obj === undefined || typeof classFunction !== "function")
+    return false;
+
+  let currPotentialPrototype = Object.getPrototypeOf(obj);
+
+  while (currPotentialPrototype !== null) {
+    if (currPotentialPrototype === classFunction.prototype) return true;
+    currPotentialPrototype = Object.getPrototypeOf(currPotentialPrototype);
+  }
+
+  return false;
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
