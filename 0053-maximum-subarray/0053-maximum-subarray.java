4c160b30aa5a4c61187412c93f5d0720a3224fda
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-07-28 11:41:30 +0530

    Time: 1 ms (100.00%), Space: 60.2 MB (14.63%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -2,11 +2,18 @@ class Solution {
     public int maxSubArray(int[] nums) {
         int sum = 0;
         int maximum = nums[0];
+        int start = 0;
+        int ansStart = -1;
+        int ansEnd = -1;
         for(int i = 0; i<nums.length;i++){
+            if(sum == 0)
+                start = i;
           sum += nums[i];
             // if the sum is greater than maximum replace it with sum
           if(sum>maximum) 
               maximum = sum;
+              ansStart = start; 
+            ansEnd = i;
             //if the sum is negative then it will erase it 
            if(sum<0) 
                 sum = 0;
