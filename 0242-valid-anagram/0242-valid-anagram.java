d320a36da8c0ca71297776a0085252e3f06886b0
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-08-08 23:26:47 +0530

    Time: 15 ms (30.84%), Space: 43.8 MB (38.52%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,16 +2,23 @@ class Solution {
 
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
-
-        int[] store = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            store[s.charAt(i) - 'a']++;
-            store[t.charAt(i) - 'a']--;
+         HashMap<Character, Integer> map = new HashMap<>();
+         for(int i = 0; i < s.length(); i++){
+             char ch = s.charAt(i);
+             map.put(ch, map.getOrDefault(ch, 0) + 1);
+         }
+        for(int i = 0; i < t.length(); i++){
+            char ch = t.charAt(i);
+            if(map.containsKey(ch) == false){
+                return false;
+            }
+            if(map.get(ch) == 1){
+                map.remove(ch);
+            } 
+            else {
+                map.put(ch, map.get(ch) - 1);
+            }
         }
-
-        for (int n : store) if (n != 0) return false;
-
-        return true;
+        return map.size() == 0;
     }
 }
