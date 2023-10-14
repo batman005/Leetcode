36238cddfc488c5796529975b4145c4c577a9ae5
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2023-10-14 23:31:21 +0530

    Time: 2 ms (82.59%), Space: 43.1 MB (77.29%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,29 +9,41 @@
  * }
  */
 class Solution {
-     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) {
-            return null;
-        }
+public ListNode mergeKLists(ListNode[] lists) {
+        int size = lists.length;
+        int interval = 1;
 
-        PriorityQueue<ListNode> queue = new PriorityQueue<>((a, b) -> a.val - b.val);
-        for (ListNode node : lists) {
-            if (node != null) {
-                queue.offer(node);
+        while (interval < size) {
+            for (int i = 0; i < size - interval; i += 2 * interval) {
+                lists[i] = merge(lists[i], lists[i + interval]);
             }
+
+            interval *= 2;
         }
 
+        return size > 0 ? lists[0] : null;
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
+        ListNode curr = dummy;
 
-        while (!queue.isEmpty()) {
-            ListNode node = queue.poll();
-            current.next = node;
-            current = current.next;
-
-            if (node.next != null) {
-                queue.offer(node.next);
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
             }
+
+            curr = curr.next;
+        }
+
+        if (l1 != null) {
+            curr.next = l1;
+        } else {
+            curr.next = l2;
         }
 
         return dummy.next;
