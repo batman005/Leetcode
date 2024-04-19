136bfed8aac99375b45735ad0279d55f7fa205a8
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2024-04-19 11:58:01 +0530

    Time: 2526 ms (5.00%), Space: 53.5 MB (8.70%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,26 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        int n = nums.length;
-        Arrays.sort(nums);
-        for(int i = 0; i < n; i++){
-                //remove duplicates:
-            if(i != 0 && nums[i] == nums[i - 1]) continue;
-            //two pointer
-            int j = i + 1;
-            int k = n - 1;
-        
-            while(j < k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum < 0){
-                    j++;
+    public List<List<Integer>> threeSum(int[] arr) {
+        int n = arr.length;
+       Set<List<Integer>> st = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            Set<Integer> hashset = new HashSet<>();
+            for (int j = i + 1; j < n; j++) {
+                //Calculate the 3rd element:
+                int third = -(arr[i] + arr[j]);
+
+                //Find the element in the set:
+                if (hashset.contains(third)) {
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], third);
+                    temp.sort(null);
+                    st.add(temp);
                 }
-                else if(sum > 0){
-                     k--;
-                }
-                else {
-                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
-                    ans.add(temp);
-                    j++;
-                    k--;
-                    
-                    //skip the duplicates:
-                    while(j < k && nums[j] == nums[j - 1]) j++;
-                    while(j < k && nums[k] == nums[k + 1]) k--;
-                }
+                hashset.add(arr[j]);
             }
         }
+
+        // store the set elements in the answer:
+        List<List<Integer>> ans = new ArrayList<>(st);
         return ans;
     }
 }
