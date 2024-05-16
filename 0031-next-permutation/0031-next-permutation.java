28ee17cae2b8974fe253c04aa43e97c2a83d60ee
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2024-05-16 16:14:44 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (30.24%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,46 +1,32 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-  int n = nums.length;
-
-  int ind = -1;
-  for (int i = n - 2; i >= 0; i--) {
-    if (nums[i] < nums[i + 1]) {
-      ind = i;
-      break;
+         int i = nums.length - 2;
+        while(i >= 0 && nums[i + 1] <= nums[i]){
+            i--;
+        }
+        if(i >= 0){
+            int j = nums.length - 1;
+            while(nums[j] <= nums[i]){
+                j--;
+            }
+            
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
     }
-  }
-
-  if (ind == -1) {
-    reverse(nums, 0, n - 1); // Reverse the entire array
-    return;
-  }
-
-  for (int i = n - 1; i > ind; i--) {
-    if (nums[i] > nums[ind]) {
-      swap(nums, i, ind);
-      break;
+    
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
-  }
-
-  // Reverse the subarray from ind + 1 to the end
-  reverse(nums, ind + 1, n - 1);
-
-  // No need to return anything as the permutation is created in-place
+    
+    public void reverse(int[] nums, int start){
+        int i = start, j = nums.length - 1;
+        while(i < j){
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
 }
-
-// Helper function to reverse a subarray
-private static void reverse(int[] nums, int start, int end) {
-  while (start < end) {
-    swap(nums, start, end);
-    start++;
-    end--;
-  }
-}
-
-// Helper function to swap elements
-private static void swap(int[] nums, int i, int j) {
-  int temp = nums[i];
-  nums[i] = nums[j];
-  nums[j] = temp;
-}
-    } 
