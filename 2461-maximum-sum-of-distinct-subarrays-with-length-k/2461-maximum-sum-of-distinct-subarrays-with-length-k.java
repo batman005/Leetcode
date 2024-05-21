d69b6533d7174332d778e758a05b712c992d2244
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2024-05-21 12:55:36 +0530

    Time: 43 ms (52.95%), Space: 57 MB (81.18%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -4,13 +4,16 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
         int i = 0, j = 0;
         while(i < nums.length){
+            //adding elements in set 
             while(j < nums.length && set.size() < k && !set.contains(nums[j])){
                 set.add(nums[j]);
                 currSum +=nums[j];
                 j++;
             }
+            //Checking size of window
             if(set.size() == k)
                 maxSum  = Math.max(currSum ,maxSum);
+            //removing previous  ele in set 
             currSum -=nums[i];
             set.remove(nums[i]);
             i++;
