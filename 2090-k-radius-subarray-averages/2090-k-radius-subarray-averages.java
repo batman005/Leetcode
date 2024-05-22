d3a4d70a1a0891d808ea3855cf67f3f7dd881311
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2024-05-22 14:42:46 +0530

    Time: 6 ms (72.50%), Space: 60.3 MB (88.38%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,35 +1,30 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
-        // When a single element is considered then its averafge will be the number itself only.
-        if (k == 0) {
+        if(k == 0){
             return nums;
         }
-
+        
         int n = nums.length;
         int[] averages = new int[n];
+        
         Arrays.fill(averages, -1);
-
-        // Any index will not have 'k' elements in it's left and right.
-        if (2 * k + 1 > n) {
+        
+        if(2 * k + 1 > n){
             return averages;
         }
-
-        // Generate 'prefix' array for 'nums'.
-        // 'prefix[i + 1]' will be sum of all elements of 'nums' from index '0' to 'i'.
-        long[] prefix = new long[n + 1];
-        for (int i = 0; i < n; ++i) {
-            prefix[i + 1] = prefix[i] + nums[i];
+        
+        //avg -> [-1, -,1,-1, -1,-1,-1,-1]
+        
+        long windowSum = 0;  //37 
+        for(int i = 0; i < (2* k + 1); i++){
+            windowSum += nums[i];
         }
-
-        // We iterate only on those indices which have atleast 'k' elements in their left and right.
-        // i.e. indices from 'k' to 'n - k'
-        for (int i = k; i < (n - k); ++i) {
-            int leftBound = i - k, rightBound = i + k;
-            long subArraySum = prefix[rightBound + 1] - prefix[leftBound];
-            int average = (int) (subArraySum / (2 * k + 1));
-            averages[i] = average;
+        averages[k] = (int)(windowSum / (2 * k + 1));
+        
+        for(int i = (2 * k + 1); i < n; i++){
+            windowSum = windowSum + nums[i] - nums[i - (2 * k + 1)];
+            averages[i - k] =  (int)(windowSum / (2 * k + 1));
         }
-
         return averages;
     }
 }
