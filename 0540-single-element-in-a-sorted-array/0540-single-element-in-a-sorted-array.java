16e2415b0ec03950903f7bc6f9fa257221b92147
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2024-06-03 21:29:40 +0530

    Time: 0 ms (100.00%), Space: 50 MB (69.09%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+       //applying binary search because it is sorted array 
+       int low = 0;
+       int high = nums.length - 2;
+        while(low <= high){
+            int mid = (low + high) >> 1;
+            if(nums[mid] == nums[mid ^ 1]){
+                low = mid + 1;
+            } else{
+                high = mid - 1;
+            }
+        }
+        return nums[low];
+    }
+}
+
+
+// in order to check for the left half
+// 1 st instance -> even index
+// 2nd instance -> odd index, i need to check on the left if the 1sr instance of that number is there or not
