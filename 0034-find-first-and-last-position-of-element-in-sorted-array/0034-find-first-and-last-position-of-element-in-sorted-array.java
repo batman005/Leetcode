d865e67c50b5ee02c0852f7a4533f2a81e90ead6
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2024-06-06 11:19:58 +0530

    Time: 0 ms (100.00%), Space: 46.1 MB (12.19%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,18 +1,48 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = -1; 
-        int last = -1;
-        for(int i= 0; i < nums.length; i ++){
-            if(nums[i] == target){
-                if(first == -1) 
-                    first = i;
-                    last = i;
-                
+        int first = firstOccurrence(nums, target);
+        if (first == -1) return new int[]{-1, -1};
+        int last = lastOccurrence(nums, target);
+        return new int[]{first, last};
+    }
+
+    public int firstOccurrence(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int first = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                first = mid;
+                end = mid - 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
-        int [] ans = new int[2];
-        ans[0] = first;
-        ans[1] = last;
-        return ans;
+        return first;
+    }
+
+    public int lastOccurrence(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int last = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                last = mid;
+                start = mid + 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return last;
     }
 }
