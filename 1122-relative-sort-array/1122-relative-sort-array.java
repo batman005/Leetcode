3c2a5d9a39e7547eb9a2b35aa1a77842c32294df
Hrithik Goswami <51878340+batman005@users.noreply.github.com>
2024-06-11 16:31:21 +0530

    Time: 5 ms (13.33%), Space: 41.9 MB (61.60%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,27 +1,25 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int maxElement = Arrays.stream(arr1).max().orElse(0);
-        int[] count = new int[maxElement + 1];
+     List<Integer> result = new ArrayList<>();
         
-        for(int element: arr1){
-            count[element]++;
-        }
-        
-        List<Integer> result = new ArrayList<>();
-        
-        for(int value: arr2){
-            while(count[value] > 0){
-                result.add(value);
-                count[value]--;
+        for(int i = 0; i < arr2.length; i++){
+            for(int j = 0; j < arr1.length; j++){
+                 if(arr1[j] == arr2[i]){
+                     result.add(arr1[j]);
+                     
+                     arr1[j] =-1;
+                 } 
             }
         }
         
-        for(int num = 0; num <= maxElement; num++){
-            while(count[num] > 0){
-                result.add(num);
-                count[num]--;
+        Arrays.sort(arr1);
+        
+        for(int i = 0; i < arr1.length;i++){
+            if(arr1[i] != -1){
+                result.add(arr1[i]);
             }
         }
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        
+        return result.stream().mapToInt(Integer:: intValue).toArray();
     }
 }
